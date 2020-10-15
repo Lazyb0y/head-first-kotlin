006d56b7ece049f4303354e668b78d755e015cde
Foysal Ahamed <thisisfoysal@gmail.com>
2020-10-15 22:35:26 +0600

    Some codes we can right in main function

--- src/chapter_01/src/main/kotlin/App.kt
+++ src/chapter_01/src/main/kotlin/App.kt
@@ -1,3 +1,29 @@
 fun main(args: Array<String>) {
-    println("Pow!")
+    var x = 3
+    val name = "Ahamed"
+
+    x *= 10
+
+    println("x is $x.")
+
+    while (x > 20) {
+        x -= 1
+        println("x is now $x.")
+    }
+
+    for (i in 1..10) {
+        x += 1
+        println("x is now $x.")
+    }
+
+    if (x == 20) {
+        println("x must be 20.")
+    } else {
+        println("x isn't 20.")
+    }
+
+    // Ahamed is the boss
+    if (name.equals("Ahamed")) {
+        println("$name Strike.")
+    }
 }
