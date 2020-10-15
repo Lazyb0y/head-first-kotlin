f4696c6060e35f5530df4d768319f45fc50bc82e
Foysal Ahamed <thisisfoysal@gmail.com>
2020-10-15 22:42:34 +0600

    A loopy example

--- src/chapter_01/src/main/kotlin/App.kt
+++ src/chapter_01/src/main/kotlin/App.kt
@@ -1,29 +1,9 @@
 fun main(args: Array<String>) {
-    var x = 3
-    val name = "Ahamed"
-
-    x *= 10
-
-    println("x is $x.")
-
-    while (x > 20) {
-        x -= 1
-        println("x is now $x.")
-    }
-
-    for (i in 1..10) {
+    var x = 1
+    println("Before the loop, x = $x.")
+    while(x < 4){
+        println("In the loop, x = $x.")
         x += 1
-        println("x is now $x.")
     }
-
-    if (x == 20) {
-        println("x must be 20.")
-    } else {
-        println("x isn't 20.")
-    }
-
-    // Ahamed is the boss
-    if (name.equals("Ahamed")) {
-        println("$name Strike.")
-    }
+    println("After the loop, x = $x.")
 }
