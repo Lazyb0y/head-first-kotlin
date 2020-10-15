600c3021ee7bbb6658d5b8e82303df49bf07124f
Foysal Ahamed <thisisfoysal@gmail.com>
2020-10-15 23:20:01 +0600

    OMG, if returning a value?

--- src/chapter_01/src/main/kotlin/App.kt
+++ src/chapter_01/src/main/kotlin/App.kt
@@ -1,10 +1,7 @@
 fun main(args: Array<String>) {
     val x = 3
     val y = 1
-    if (x > y) {
-        println("x is greater than y")
-    } else {
-        println("x is not greater than y")
-    }
-    println("This line runs no matter what")
+
+    println(if (x > y) "x is greater than y" else "x is not greater than y.")
+    println("This line runs no matter what.")
 }
