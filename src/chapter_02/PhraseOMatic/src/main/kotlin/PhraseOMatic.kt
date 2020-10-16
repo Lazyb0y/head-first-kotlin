ca31e85742f01bf77161d0c8c2447705c1ceecf5
Foysal Ahamed <thisisfoysal@gmail.com>
2020-10-16 13:38:03 +0600

    Moved chapter 2 initial codes to a new function

--- src/chapter_02/PhraseOMatic/src/main/kotlin/PhraseOMatic.kt
+++ src/chapter_02/PhraseOMatic/src/main/kotlin/PhraseOMatic.kt
@@ -1,4 +1,8 @@
 fun main(args: Array<String>) {
+    initialCodeBlocks()
+}
+
+fun initialCodeBlocks() {
     val wordArray1 = arrayOf("24/7", "multi-tier", "B-to-B", "dynamic", "pervasive")
     val wordArray2 = arrayOf("empowered", "leveraged", "aligned", "targeted")
     val wordArray3 = arrayOf("process", "paradigm", "solution", "portal", "vision")
