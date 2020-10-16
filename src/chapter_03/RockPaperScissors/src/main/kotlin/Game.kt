74951f05f50036c87798be4fd7d73bb60ed8500c
Foysal Ahamed <thisisfoysal@gmail.com>
2020-10-16 15:37:12 +0600

    Taking choice from the user

--- src/chapter_03/RockPaperScissors/src/main/kotlin/Game.kt
+++ src/chapter_03/RockPaperScissors/src/main/kotlin/Game.kt
@@ -2,8 +2,20 @@ fun main(args: Array<String>) {
     val options = arrayOf("Rock", "Paper", "Scissors")
     val gameChoice = getGameChoice(options)
     println("Game choice: $gameChoice")
+
+    val userChoice = getUserChoice(options)
+    println("User choice: $userChoice")
 }
 
 fun getGameChoice(optionsParam: Array<String>): String {
     return optionsParam[(Math.random() * optionsParam.size).toInt()]
+}
+
+fun getUserChoice(optionsParam: Array<String>): String {
+    print("Please enter one of the following:")
+    for (item in optionsParam) print(" $item")
+    println(".")
+
+    val userInput = readLine()
+    return userInput.toString()
 }
