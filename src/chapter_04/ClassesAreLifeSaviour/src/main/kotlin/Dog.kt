0c11143b96eb18133a8380bd7ed68d9b00de7e99
Foysal Ahamed <thisisfoysal@gmail.com>
2020-10-18 18:25:57 +0600

    Initializer blocks

--- src/chapter_04/ClassesAreLifeSaviour/src/main/kotlin/Dog.kt
+++ src/chapter_04/ClassesAreLifeSaviour/src/main/kotlin/Dog.kt
@@ -2,6 +2,14 @@ class Dog(val name: String, var weight: Int, breed_param: String) {
     var activities = arrayOf("Walks")
     val breed = breed_param.toUpperCase()
 
+    init {
+        println("Dog $name has been created.")
+    }
+
+    init {
+        println("The breed is $breed.")
+    }
+
     fun bark() {
         println(if (weight < 20) "Yip!" else "Woof!")
     }
