41b5ba9be5838fb4ee2c0c041fb0d407e5a3fec1
Foysal Ahamed <thisisfoysal@gmail.com>
2020-10-18 18:12:02 +0600

    Flexible property initialization

--- src/chapter_04/ClassesAreLifeSaviour/src/main/kotlin/Dog.kt
+++ src/chapter_04/ClassesAreLifeSaviour/src/main/kotlin/Dog.kt
@@ -1,4 +1,7 @@
-class Dog(val name: String, var weight: Int, val breed: String) {
+class Dog(val name: String, var weight: Int, breed_param: String) {
+    var activities = arrayOf("Walks")
+    val breed = breed_param.toUpperCase()
+
     fun bark() {
         println(if (weight < 20) "Yip!" else "Woof!")
     }
