bbe8c578b9f1c65b183a3525341f6bbff1c03309
Foysal Ahamed <thisisfoysal@gmail.com>
2020-10-23 14:37:52 +0600

    added remaining code

--- src/chapter_04/ClassesAreLifeSaviour/src/main/kotlin/App.kt
+++ src/chapter_04/ClassesAreLifeSaviour/src/main/kotlin/App.kt
@@ -1,7 +1,17 @@
 fun main(args: Array<String>) {
-    var myDog = Dog("Fido", 70, "Mixed")
-    println("My dog's name is ${myDog.name}.")
+    val myDog = Dog("Fido", 70, "Mixed")
     myDog.bark()
+
+    myDog.weight = 75
+    println("Weight in Kgs is ${myDog.weightInKgs}")
+
+    myDog.weight = -2
+    println("Weight is ${myDog.weight}")
+
+    myDog.activities = arrayOf("Walks", "Fetching balls", "Frisbee")
+    for (item in myDog.activities) {
+        println("My dog enjoys $item")
+    }
 
     var dogs = arrayOf(
         Dog("Fido", 70, "Mixed"),
