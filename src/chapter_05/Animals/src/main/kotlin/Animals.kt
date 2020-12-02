1b5c07677e204fac8e66b5410039bfec3634b83b
Foysal Ahamed <thisisfoysal@gmail.com>
2020-12-02 20:48:47 +0600

    Removed Animals class and moved main method to the outside of the class

--- src/chapter_05/Animals/src/main/kotlin/Animals.kt
+++ src/chapter_05/Animals/src/main/kotlin/Animals.kt
@@ -1,17 +1,15 @@
-class Animals {
-    fun main(args: Array<String>) {
+fun main(args: Array<String>) {
 
-        val vet = Vet()
-        val wolf = Wolf()
-        val hippo = Hippo()
+    val vet = Vet()
+    val wolf = Wolf()
+    val hippo = Hippo()
 
-        val animals = arrayOf(hippo, wolf)
-        for (item in animals) {
-            item.roam()
-            item.eat()
-        }
-
-        vet.giveShot(wolf)
-        vet.giveShot(hippo)
+    val animals = arrayOf(hippo, wolf)
+    for (item in animals) {
+        item.roam()
+        item.eat()
     }
+
+    vet.giveShot(wolf)
+    vet.giveShot(hippo)
 }
