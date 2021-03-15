5cef3ace8d6d1a806f2f499bb9dba9f34e5aa2a7
Foysal Ahamed <thisisfoysal@gmail.com>
2021-03-15 20:56:26 +0600

    Generic add/remove

---
+++ src/chapter_10/Generics/src/PetOwner.kt
@@ -0,0 +1,22 @@
+fun main(args: Array<String>) {
+
+    val catFuzz = Cat("Fuzz Lightyear")
+    val catKatsu = Cat("Katsu")
+    val fishFinny = Fish("Finny McGraw")
+
+    val catOwner = PetOwner(catFuzz)
+    catOwner.add(catKatsu)
+}
+
+
+class PetOwner<T : Pet>(t: T) {
+    val pets = mutableListOf(t)
+
+    fun add(t: T) {
+        pets.add(t)
+    }
+
+    fun remove(t: T) {
+        pets.remove(t)
+    }
+}
