507dadb80a01265027e79588eeea2e5b610a849b
Foysal Ahamed <thisisfoysal@gmail.com>
2021-04-04 16:59:23 +0600

    Lamda signatures

--- src/chapter_11/Lambdas/src/Lambdas.kt
+++ src/chapter_11/Lambdas/src/Lambdas.kt
@@ -1,3 +1,15 @@
+fun convert(x: Double, converter: (Double) -> Double): Double {
+    val result = converter(x)
+    println("$x is converted to $result")
+    return result
+}
+
+fun convertFive(converter: (Int) -> Double): Double {
+    val result = converter(5)
+    println("5 is converted to $result")
+    return result
+}
+
 fun main() {
     val addFive = { x: Int -> x + 5 }
     println("Pass 6 to addFive: ${addFive(6)}")
@@ -14,4 +26,7 @@ fun main() {
 
     val myLambda: () -> Unit = { println("Hi!") }
     myLambda()
+
+    convert(20.0) { it * 1.8 + 32 }
+    convertFive { it * 1.8 + 32 }
 }
